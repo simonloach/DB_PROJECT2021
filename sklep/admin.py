c590a50f4122a1812dc356db632f0e2b1fa896f2
simonloach <mrtheyouis@gmail.com>
2021-01-13 17:34:15 +0100

    admin

--- sklep/admin.py
+++ sklep/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Categorie, Client,Manufacturer,ManufacturersCategorie,Order,OrderedProduct,Product
 
-
+admin.site.register(Categorie)
+admin.site.register(Client)
+admin.site.register(Manufacturer)
+admin.site.register(ManufacturersCategorie)
+admin.site.register(Order)
+admin.site.register(OrderedProduct)
+admin.site.register(Product)

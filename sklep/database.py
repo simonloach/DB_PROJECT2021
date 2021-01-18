69be6e26484007ab014751ff0a7bda21f1fcc8ae
simonloach <mrtheyouis@gmail.com>
2021-01-18 23:04:28 +0100

    new db getters

--- sklep/database.py
+++ sklep/database.py
@@ -2,8 +2,8 @@ import psycopg2
 
 
 #get rid of hardcoding later
-username = 'postgres'
-password = 'root'
+username = 'g12'
+password = 'gwao6hn4'
 host = 'localhost'
 port = 5432
 
@@ -51,9 +51,54 @@ def getCategoryTree(cid):
         print(error)
 
     finally:
+        if conn is not None:
+            conn.close()
+
+def getBestSelling(days,count):
+    conn = None
+    days = str(days)
+    try:
+        conn = getConn()
+        cur = conn.cursor()
+        with cur:
+            cur.execute("select * from product join"
+                        " (select op.pid from ordered_product op inner join"
+                        " \"order\" o  on op.oid = o.oid   "
+                        "where o.order_placed_date > current_date - interval %s day  "
+                        "group by op.pid order by count(op.pid) desc limit %s) m on m.pid = product.pid"
+                        " where product.no_instock > 0;"
+                        ,[days,count])
+
+            return cur.fetchall()
+
+    except (psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if cur is not None:
+            cur.close()
+
+
+def getByLowestInStock(count):
+    conn = None
+    try:
+        conn = getConn()
+        cur = conn.cursor()
+        with cur:
+            cur.execute("select * from product where no_instock > 0 order by no_instock asc limit %s;",[count])
+
+            return cur.fetchall()
+
+    except (psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
         if cur is not None:
             cur.close()
 
 
 
+
+
+print(getBestSelling(10,10))
 
